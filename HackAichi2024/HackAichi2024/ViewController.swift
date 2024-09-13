ac80dd8e1cc896b9350b10efb8db005b4bd054d7
Aoyama Jun <g.exst.aom.j@gmail.com>
2024-09-13 17:55:07 +0900

    最低限のiOSプロジェクトを追加

---
+++ HackAichi2024/HackAichi2024/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HackAichi2024
+//
+//  Created by jun on 2024/09/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .blue
+    }
+
+
+}
+
