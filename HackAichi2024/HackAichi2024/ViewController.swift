40c57a959abe1f0595e2e199fbc2faaf557cf30c
Aoyama Jun <g.exst.aom.j@gmail.com>
2024-09-15 16:42:13 +0900

    リアクションボタンを配置

--- HackAichi2024/HackAichi2024/ViewController.swift
+++ HackAichi2024/HackAichi2024/ViewController.swift
@@ -9,17 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var questionSendView: UIView!
+    var questionSendView: QuestionSendView!
+    var goodButton: ReactionButton!
+    var badButton: ReactionButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         self.view.backgroundColor = .systemBackground
         
+        goodButton = ReactionButton(type: .good)
+        self.view.addSubview(goodButton)
+        badButton = ReactionButton(type: .bad)
+        self.view.addSubview(badButton)
+        
         questionSendView = QuestionSendView()
         self.view.addSubview(questionSendView)
         
         NSLayoutConstraint.activate([
+            goodButton.widthAnchor.constraint(equalToConstant: 70),
+            goodButton.heightAnchor.constraint(equalToConstant: 40),
+            goodButton.trailingAnchor.constraint(equalTo: view.centerXAnchor, constant: -20),
+            goodButton.bottomAnchor.constraint(equalTo: questionSendView.topAnchor, constant: -15),
+            
+            badButton.widthAnchor.constraint(equalTo: goodButton.widthAnchor),
+            badButton.heightAnchor.constraint(equalTo: goodButton.heightAnchor),
+            badButton.leadingAnchor.constraint(equalTo: view.centerXAnchor, constant: 20),
+            badButton.bottomAnchor.constraint(equalTo: goodButton.bottomAnchor),
+            
             questionSendView.heightAnchor.constraint(equalToConstant: 45),
             questionSendView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10),
             questionSendView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
